12d05188e791b957da90c59e3428ff7fc51a74cf
Tisdale Fry <tisdalefry@gmail.com>
2020-02-21 22:46:32 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -9,11 +9,13 @@ def subtraction(num1, num2)
 end
 
 def division(num1, num2)
-
+  quotient = (num1/num2)
+  return quotient
 end
 
 def multiplication(num1, num2)
-
+  product = (num1*num2)
+  return product
 end
 
 def modulo(num1, num2)
